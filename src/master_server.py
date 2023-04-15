f34d50eab1513a653c6b4a634cc113c10226d001
PramodRaoB <pramod.b@research.iiit.ac.in>
2023-04-16 03:44:29 +0530

    Add proto and master skeleton

---
+++ src/master_server.py
@@ -0,0 +1,24 @@
+from concurrent import futures
+import logging
+
+import grpc
+import hybrid_dfs_pb2
+import hybrid_dfs_pb2_grpc
+
+
+class MasterToClientServicer(hybrid_dfs_pb2_grpc.MasterToClientServicer):
+    """Provides methods that implements functionality of HybridDFS Master server"""
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
+    hybrid_dfs_pb2_grpc.add_MasterToClientServicer_to_server(
+        MasterToClientServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
